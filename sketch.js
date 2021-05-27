6614ad559529983bca6fcbcf2d23ffcd60d27663
alagu-sketchart <74680709+alagu-sketchart@users.noreply.github.com>
2021-05-26 23:08:07 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,85 +1,61 @@
-const Engine = Matter.Engine;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const World = Matter.World;
-
-var helicopterIMG, helicopterSprite, packageSprite ,packageIMG;
-var world, engine;
-var ground, packageBody, box1, box2, box3;
-
-function preload(){
-helicopterIMG = loadImage("helicopter.png");
-packageIMG = loadImage("package.png");
-}
-
-function setup(){
-  createCanvas(700,800);
-  rectMode(CENTER);
-  engine = Engine.create();
-  world = engine.world;
-
-  packageSprite = createSprite(width/2, 100, 10,10);
-	packageSprite.addImage(packageIMG);
-	packageSprite.scale = 0.2;
-
-	helicopterSprite = createSprite(width/2, 100, 10,10);
-	helicopterSprite.addImage(helicopterIMG);
-  helicopterSprite.scale = 0.6
-  
-  ground = createSprite(width/2, height-150, width, 10);
-  ground.shapeColor = color(255);
-
-  box1 = createSprite(275, height-195, 10, 75);
-  box1.shapeColor = color("red");
-
-  box2 = createSprite(width/2, height-157, 150, 10);
-  box2.shapeColor = color("red");
-
-  box3 = createSprite(425, height-195, 10, 75);
-  box3.shapeColor = color("red");
-
-
-var ground_p = {
-    isStatic : true 
-}
-
-  ground = Bodies.rectangle(width/2, 650, 100, 20, ground_p);
-
-var box2_p = {
-    isStatic : true
-}
-
-  box2 = Bodies.rectangle(width/2, 770, 150, 10, box2_p);
-  World.add(world, ground, packageBody, box2);
-
-  var packageBody_p = {
-    restitution: 0.5 
-}
-
-packageBody = Bodies.circle(width/2,100,20,packageBody_p);
-}
-
-function draw(){
-  background(0);
-  Engine.update(engine);
-  packageSprite.y = packageBody.position.y
-  packageSprite.x = packageBody.position.x
-
-  
-  drawSprites();
-}
-
-function keyPressed() {
-  if (keyCode === DOWN_ARROW) {
-   
-     var packageBody_p = {
-     restitution: 0.5
-   }
-
-
-   packageBody = Bodies.circle(width/2,100,20,packageBody_p);
-   World.add(world,packageBody);
-   }
+ //Create variables here
+ var dog, happyDog, database, foodS, foodStock;
+ 
+ function preload(){
+   //load images here
+   Standdog = loadImage("dog.png")
+   happyDog = loadImage("Happydog.png")
+ }
+ 
+ function setup() {
+    createCanvas(500,500);
+    dog = createSprite(250,300,150,150);
+    dog.addImage(Standdog)
+    
+    dog.scale = 0.25
+    database = firebase.database();
+    foodStock = database.ref('food');
+    foodStock.on("value",readStock);  
+ }
+ 
+ 
+ function draw() {  
+ background(50, 140, 85);
+ 
+ if(keyWentDown(UP_ARROW)){
+   writeStock(foodS)
+   dog.addImage(happyDog)
+ }
+ else{
+   dog.addImage(Standdog)
  }
 
+   drawSprites();
+   //add styles here
+   textSize(13);
+   fill("white")
+   text("Note: Press Up arrow key to feed the Dog",115,30)
  
+   textSize(15);
+   fill("white")
+   text(" Food Remaining : " + foodS,180,180);
+ 
+ }
+ 
+ function readStock(data){
+ foodS = data.val();
+
+ }
+ 
+ function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1
+  }
+
+   database.ref('/').update({
+    //food:x = 20 //<-This is for reseting the food back to normal.
+    food:x = x
+   })
+   }
